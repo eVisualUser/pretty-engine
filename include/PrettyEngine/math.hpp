af6018612334ca8ced0e9aae54854f37e9407b21
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-12-06 23:32:56 +0100

    Remove unused variable

--- include/PrettyEngine/math.hpp
+++ include/PrettyEngine/math.hpp
@@ -3,14 +3,6 @@
 
 #include <glm/glm.hpp>
 
-namespace PrettyEngine {
-	static glm::vec3 AABBClosestPoint(glm::vec3 minA, glm::vec3 maxA, glm::vec3 minB, glm::vec3 maxB) {
-		glm::vec3 out(0.0f, 0.0f, 0.0f);
-
-		
-
-		return out;
-	}
-}
+namespace PrettyEngine {}
 
 #endif
